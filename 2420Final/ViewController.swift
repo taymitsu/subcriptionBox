53cf15c607ec26832817a2f63dcebe85443b8bd2
Tay <tmitsumori96@gmail.com>
2022-05-19 13:34:39 -0700

    Initial Commit

---
+++ 2420Final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2420Final
+//
+//  Created by Tay Mitsumori on 5/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
